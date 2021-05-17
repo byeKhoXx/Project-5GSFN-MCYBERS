d3e2c669bb8a0cce53d59da1337196218575e03c
albert752 <azemar.albert@gmail.com>
2021-05-17 02:04:36 +0200

    Added proxy

--- proxy/configs/web_server_route.py
+++ proxy/configs/web_server_route.py
@@ -23,7 +23,6 @@ import time
 
 logger = logging.getLogger(__name__)
 WEBSERVER_IP = "10.0.5.4" # IP of the webserver that is to be protected
-PATH_TO_BLOCK_LIST = "/home/albert752/block_list.txt" # Path to the blacklist
 CACHE = {"time": 0, "data": None} # Cache variable for the website
 MAX_CACHE_TIME = 10 # Maximum number of seconds without refreshing the cache
 
@@ -53,29 +52,21 @@ class WebServerPlugin(HttpWebServerBasePlugin):
         :param request: HttpParser object
         :return: cached webpage from the original webserver
         """
-        # Opens the blacklist and parses all the instances
-        with open(PATH_TO_BLOCK_LIST, 'r') as fp:
-            ips = json.load(fp)
-            fp.close()
 
-        # Only responds if the IP is not present or, in case that it is present, if the blocking period
-        # has expired.
-        if self.client.addr[0] not in list(ips.keys()) or time.time() > ips[self.client.addr[0]]:
-
-            # Check if the cache is old
-            if CACHE['time'] + MAX_CACHE_TIME < time.time():
-                # If the cache is old, request again the webpage
-                response = requests.get("http://" + WEBSERVER_IP).content
-                CACHE['time'] = time.time()
-                CACHE['data'] = response
-                print("******************** serving from webserver **********************")
-            else:
-                # If not, announce that it is using the cache only for demonstration purposes
-                print("******************** serving from cache **********************")
-            if request.path == b'/':
-                # Finally fill the response with the webpage data
-                self.client.queue(memoryview(build_http_response(
-                    httpStatusCodes.OK, body=CACHE['data'])))
+        # Check if the cache is old
+        if CACHE['time'] + MAX_CACHE_TIME < time.time():
+            # If the cache is old, request again the webpage
+            response = requests.get("http://" + WEBSERVER_IP).content
+            CACHE['time'] = time.time()
+            CACHE['data'] = response
+            print("******************** serving from webserver **********************")
+        else:
+            # If not, announce that it is using the cache only for demonstration purposes
+            print("******************** serving from cache **********************")
+        if request.path == b'/':
+            # Finally fill the response with the webpage data
+            self.client.queue(memoryview(build_http_response(
+                httpStatusCodes.OK, body=CACHE['data'])))
 
 
     def on_websocket_open(self) -> None:
